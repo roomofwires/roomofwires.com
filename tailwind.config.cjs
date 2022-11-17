fd0d036b68e66d11445538d52994b2d83bd75500
roomofwires <acturneruk@icloud.com>
2022-11-17 10:26:47 +0000

    Formatting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'gold': '#fdd835',
-				'camo': '#4c4a3d',
-				'sand': '#908d77',
-			},
-		},
-	},
-	corePlugins: {
-		aspectRatio: false,
-	},
-	plugins: [
-		require('@tailwindcss/aspect-ratio'),
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        gold: "#fdd835",
+        camo: "#4c4a3d",
+        sand: "#908d77",
+      },
+    },
+  },
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
