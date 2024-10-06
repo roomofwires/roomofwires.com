361f2b4a4d04557dc871d2b48e346daaa71d0c8a
roomofwires <acturneruk@icloud.com>
2024-10-06 15:02:15 +0100

    Run global Prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,10 +12,14 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://roomofwires.com",
-  integrations: [tailwind(), alpinejs(),     sitemap({
-    filter: (page) =>
-      page !== 'https://roomofwires.com/4t_thieves/' &&
-      page !== 'https://roomofwires.com/dasf/' &&
-      page !== 'https://roomofwires.com/stationzero/',
-  }),],
+  integrations: [
+    tailwind(),
+    alpinejs(),
+    sitemap({
+      filter: (page) =>
+        page !== "https://roomofwires.com/4t_thieves/" &&
+        page !== "https://roomofwires.com/dasf/" &&
+        page !== "https://roomofwires.com/stationzero/",
+    }),
+  ],
 });
